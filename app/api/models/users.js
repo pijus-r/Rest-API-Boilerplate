b860072e95590edffc2be6497cc8c3733b4e52d3
Pijus Rancevas <pijus.rancevas@gmail.com>
2021-01-11 00:18:54 +0200

    Formatted

--- app/api/models/users.js
+++ app/api/models/users.js
@@ -24,7 +24,7 @@ const UserSchema = new Schema({
 });
 
 // Hash user password before saving into database
-UserSchema.pre('save', function (next)  {
+UserSchema.pre('save', function (next) {
   this.password = bcrypt.hashSync(this.password, saltRounds);
   next();
 });
